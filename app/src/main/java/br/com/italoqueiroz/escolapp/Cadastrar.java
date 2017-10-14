13e8fbd335ddefd339724396ed62439c1a9863e5
batutista <batutista14@gmail.com>
2017-10-13 22:39:21 -0300

    Initial commit

---
+++ app/src/main/java/br/com/italoqueiroz/escolapp/Cadastrar.java
@@ -0,0 +1,71 @@
+package br.com.italoqueiroz.escolapp;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class Cadastrar extends AppCompatActivity {
+
+    private EditText txtUser;
+    private EditText txtPassword;
+    private EditText txtPassword2;
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastrar);
+
+        txtUser = (EditText) findViewById(R.id.cadastrar_login);
+        txtPassword = (EditText) findViewById(R.id.cadastrar_senha);
+        txtPassword2 = (EditText) findViewById(R.id.cadastrar_senha_2);
+
+        mAuth = FirebaseAuth.getInstance();
+
+    }
+
+    public void criarConta (View view){
+
+        txtUser = (EditText) findViewById(R.id.cadastrar_login);
+        txtPassword = (EditText) findViewById(R.id.cadastrar_senha);
+        txtPassword2 = (EditText) findViewById(R.id.cadastrar_senha_2);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        if(txtPassword.getText().toString().equals(txtPassword2.getText().toString())){
+            mAuth.createUserWithEmailAndPassword(txtUser.getText().toString(), txtPassword2.getText().toString())
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                Toast.makeText(Cadastrar.this, "Cadastro realizado com sucesso", Toast.LENGTH_LONG).show();
+                                finish();
+                            } else {
+                                Toast.makeText(Cadastrar.this, "Cadastro inválido", Toast.LENGTH_LONG).show();
+                            }
+
+                        }
+                    });
+        }else{
+            Toast.makeText(Cadastrar.this, "Senha Inválida", Toast.LENGTH_LONG).show();
+        }
+
+
+    }
+
+}
---
+++ app/src/main/java/br/com/italoqueiroz/escolapp/MainActivity.java
@@ -0,0 +1,78 @@
+package br.com.italoqueiroz.escolapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText txtUser;
+    private EditText txtPassword;
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        txtUser = (EditText) findViewById(R.id.acessar_login);
+        txtPassword = (EditText) findViewById(R.id.acessar_senha);
+
+        mAuth = FirebaseAuth.getInstance();
+
+
+
+
+
+        Button botaoAcessar = (Button) findViewById(R.id.buttonAcessar);
+        botaoAcessar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mAuth.signInWithEmailAndPassword(txtUser.getText().toString(), txtPassword.getText().toString())
+                        .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if(!task.isSuccessful()){
+                                    Toast.makeText(MainActivity.this, "Usuario ou senha inválido", Toast.LENGTH_LONG).show();
+                                }else {
+                                    FirebaseUser user = task.getResult().getUser();
+                                    if(user != null){
+                                        Intent cadastrar = new Intent(MainActivity.this, Acessar.class);
+                                        startActivity(cadastrar);
+                                        finish();
+                                    }
+                                }
+                            }
+                        });
+
+            }
+        });
+
+        Button botaoCadastrar = (Button) findViewById(R.id.buttonCadastrar);
+        botaoCadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent acessar = new Intent(MainActivity.this, Cadastrar.class);
+                startActivity(acessar);
+            }
+        });
+
+
+
+
+    }
+
+}
